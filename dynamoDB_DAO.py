2a3f6ddabfd17d80b3ac68d9748a8a2bcee9e95a
Vlad Koretsky <vlad.koretsky@narvar.com>
2017-12-26 13:09:29 -0800

    First-cut of standardize error response on Customer Preferences API. Added PUT support and basic PUT vs. POST validation. Make internal methods private.

--- dynamoDB_DAO.py
+++ dynamoDB_DAO.py
@@ -1,7 +1,6 @@
 import boto3
 import datetime
 import json
-#from operator import itemgetter
 import traceback
 
 cust_table_name = 'customer-preferences-dev'
@@ -14,14 +13,14 @@ NOTIFICATION_PREF_EMAIL = 'EMAIL'
 
 VALIDATION_MSG_CUSTOMER_ID = 'Invalid input: missing required [customer_id] parameter!'
 VALIDATION_MSG_RETAILER_MONIKER = 'Invalid input: missing required [retailer] parameter in customer_preferences!'
-VALIDATION_MSG_STALE_DATA_MODIFICATION = 'Stale data modification: the record you are trying to update has been updated by another process. Please refer to modified_datetime field in customer_preferences in the response for correct value to use.'
+VALIDATION_MSG_STALE_DATA_MODIFICATION = 'Stale data modification: the record you are trying to update has been updated by another process. Please refer to [modified_datetime] field in customer_preferences in the response for correct value to use.'
 VALIDATION_MSG_CUST_PREFS = 'Invalid input: missing required [customer_preferences] section parameter in request body.'
 VALIDATION_MSG_FIRST_NAME = 'Invalid input: missing required parameter [first_name] in customer_preferences.'
 VALIDATION_MSG_LAST_NAME = 'Invalid input: missing required parameter [last_name] in customer_preferences.'
 VALIDATION_MSG_NOTIFICATION_PREFS = 'Invalid input: missing required [notification_pref] section parameter in customer_preferences.'
 VALIDATION_MSG_NOTIFICATION_PREFS_DETAILS = 'Invalid input: missing required [notification_pref_details] section parameter in customer_preferences.'
 VALIDATION_MSG_NOTIFICATION_PREFS_AND_DETAILS_MISMATCH_LENGTH = 'Invalid input: channels in [notification_pref] must match with their [notification_pref_details].'
-VALIDATION_MSG_NOTIFICATION_PREFS_AND_DETAILS_MISMATCH_ITEM = 'Invalid input: missing customer [notification_pref] details for: '
+VALIDATION_MSG_NOTIFICATION_PREFS_AND_DETAILS_MISMATCH_ITEM = 'Invalid input: missing customer [notification_pref_details] for: '
 VALIDATION_MSG_NOTIFICATION_PREFS_CHANNEL = 'Invalid input: invalid notification preference value. Supported values are: ' + ''.join([NOTIFICATION_PREF_SMS, keyDelim, NOTIFICATION_PREF_FB, keyDelim, NOTIFICATION_PREF_EMAIL])
 VALIDATION_MSG_ADDRESS = 'Invalid input: missing required [address] section parameter in customer_preferences.'
 VALIDATION_MSG_ADDRESS_STREET1 = 'Invalid input: missing required parameter [street_1] in address.'
@@ -29,14 +28,23 @@ VALIDATION_MSG_ADDRESS_CITY = 'Invalid input: missing required parameter [city] 
 VALIDATION_MSG_ADDRESS_STATE = 'Invalid input: missing required parameter [state] in address.'
 VALIDATION_MSG_ADDRESS_ZIP = 'Invalid input: missing required parameter [zip] in address.'
 VALIDATION_MSG_ADDRESS_COUNTRY = 'Invalid input: missing required parameter [country] in address.'
+VALIDATION_MSG_POST_DATA_EXISTS = 'Invalid input: you are trying to submit a POST request on an existing customer_preference. Use PUT instead.'
+VALIDATION_MSG_PUT_DATA_NOT_EXISTS = 'Invalid input: you are tyring to submit a PUT request on a resource that does not exist. Use POST instead.'
 
 class InputValidationException(Exception):
     # Server Validation Exception
     responseCode = 400
-    messages = []
-    def __init__(self, err_msgs):
+    response_messages = []
+    def __init__(self, response_messages):
         Exception.__init__(self)
-        self.messages = err_msgs
+        self.response_messages = response_messages
+
+class ResponseMessage:
+    def __init__(self, level, code, field, message):
+        self.level = level
+        self.code = code
+        self.field = field
+        self.message = message
 
 def method_get(event, context):
     exception = None
@@ -46,10 +54,10 @@ def method_get(event, context):
         error_msgs = []
         retailer_moniker = event['pathParameters']['retailer_moniker']
         customer_id = event['pathParameters']['customer_id']
-        if not retailer_moniker:
-            error_msgs.append(VALIDATION_MSG_RETAILER_MONIKER)
+        if not retailer_moniker or retailer_moniker == 'narvar-speedee':
+            error_msgs.append(ResponseMessage('ERROR', None, 'retailer_name', VALIDATION_MSG_RETAILER_MONIKER))
         if not customer_id:
-            error_msgs.append(VALIDATION_MSG_CUSTOMER_ID)
+            error_msgs.append(ResponseMessage('ERROR', None, 'customer_id', VALIDATION_MSG_CUSTOMER_ID))
         if len(error_msgs) > 0:
             raise InputValidationException(error_msgs)
     except InputValidationException as ivrme:
@@ -57,22 +65,22 @@ def method_get(event, context):
 
     customer_preferences = {}
     if exception != None:
-        return makeResponse(customer_preferences, exception, None)
+        return __makeResponse(customer_preferences, exception, None)
     # END OF: Basic input validation
 
-    customerKey = makeKey(retailer_moniker, customer_id)
+    customerKey = __makeKey(retailer_moniker, customer_id)
 
     result = {}
     try:
-        result = findCustomerPreference(customerKey)
-        customer_preferences = makeCustomerDetails(result)
+        result = __findCustomerPreference(customerKey)
+        customer_preferences = __makeCustomerDetails(result)
     except Exception as e:
         exception = e # Preserve exception for the response.
         # TODO: Log the exception.
 
-    return makeResponse(customer_preferences, exception, None)
+    return __makeResponse(customer_preferences, exception, None)
 
-def method_post(event, context):
+def method_post_put(event, context):
     exception = None
     customer_preferences = {}
 
@@ -81,41 +89,41 @@ def method_post(event, context):
         retailer_moniker = event['pathParameters']['retailer_moniker']
         customer_id = event['pathParameters']['customer_id']
         if not retailer_moniker:
-            error_msgs.append(VALIDATION_MSG_RETAILER_MONIKER)
+            error_msgs.append(ResponseMessage('ERROR', None, 'retailer', VALIDATION_MSG_RETAILER_MONIKER))
         if not customer_id:
-            error_msgs.append(VALIDATION_MSG_CUSTOMER_ID)
+            error_msgs.append(ResponseMessage('ERROR', None, 'customer_id', VALIDATION_MSG_CUSTOMER_ID))
         if len(error_msgs) > 0:
             raise InputValidationException(error_msgs)
 
         if not event or not event.get('body') or not event.get('body'):
-            error_msgs.append(VALIDATION_MSG_CUST_PREFS)
+            error_msgs.append(ResponseMessage('ERROR', None, 'customer_preferences', VALIDATION_MSG_CUST_PREFS))
             raise InputValidationException(error_msgs)
 
         cust_prefs = json.loads(event.get('body')).get('customer_preferences')
-        validateCustomerPreferences(cust_prefs)
+        __validateCustomerPreferences(cust_prefs)
     except Exception as e:
         exception = e
         # TODO: Log exception here.
 
     if exception != None:
-        return makeResponse(customer_preferences, exception, None)
+        return __makeResponse(customer_preferences, exception, None)
 
     try:
-        saveCustomerPreference(retailer_moniker, customer_id, cust_prefs)
+        __saveCustomerPreference(retailer_moniker, customer_id, cust_prefs, event.get('httpMethod'))
     except Exception as e:
         exception = e # Preserve exception for later response.
         # TODO: Log exception here.
 
-    customerKey = makeKey(retailer_moniker, customer_id)
-    search_result = findCustomerPreference(customerKey)
-    customer_preferences = makeCustomerDetails(search_result)
+    customerKey = __makeKey(retailer_moniker, customer_id)
+    search_result = __findCustomerPreference(customerKey)
+    customer_preferences = __makeCustomerDetails(search_result)
 
-    return makeResponse(customer_preferences, exception, None)
+    return __makeResponse(customer_preferences, exception, None)
 
-def saveCustomerPreference(retailer_moniker, customer_id, cust_preferences):
+def __saveCustomerPreference(retailer_moniker, customer_id, cust_preferences, httpMethod):
     db = boto3.client('dynamodb');
 
-    id = makeKey(retailer_moniker, customer_id)
+    id = __makeKey(retailer_moniker, customer_id)
 
     modified_datetime = cust_preferences['modified_datetime']
     if not modified_datetime:
@@ -124,19 +132,32 @@ def saveCustomerPreference(retailer_moniker, customer_id, cust_preferences):
     created_datetime = modified_datetime
 
     # Find if the record already exists and perform some checks:
-    dbVersion = findCustomerPreference(id)
-    if(dbVersion):
-        # Overwrite retailer_moniker, customer_id and created_datetime values in the request (modification of these is not allowed):
-        retailer_moniker = dbVersion.get('retailer_moniker')
-        customer_id = dbVersion.get('customer_id')
-        created_datetime = dbVersion.get('created_datetime')
+    dbRec = __findCustomerPreference(id)
 
-        if dbVersion.get('modified_datetime') != modified_datetime:
+    error_msgs = []
+    if(httpMethod == 'POST' and dbRec):
+        error_msgs.append(ResponseMessage('ERROR', None, None, VALIDATION_MSG_POST_DATA_EXISTS))
+        raise InputValidationException(error_msgs)
+    elif(httpMethod == 'PUT' and not dbRec):
+        error_msgs.append(ResponseMessage('ERROR', None, None, VALIDATION_MSG_PUT_DATA_NOT_EXISTS))
+        raise InputValidationException(error_msgs)
+
+    if(dbRec):
+        # Overwrite retailer_moniker, customer_id and created_datetime values in the request (modification of these is not allowed):
+        retailer_moniker = dbRec.get('retailer_moniker')
+        customer_id = dbRec.get('customer_id')
+        created_datetime = dbRec.get('created_datetime')
+
+        if dbRec.get('modified_datetime') != modified_datetime:
             error_msgs = []
-            error_msgs.append(VALIDATION_MSG_STALE_DATA_MODIFICATION)
+            error_msgs.append(ResponseMessage('ERROR', None, 'modified_datetime', VALIDATION_MSG_STALE_DATA_MODIFICATION))
             raise InputValidationException(error_msgs)
         else:
             modified_datetime = datetime.datetime.utcnow().isoformat()
+
+    __save(db, id, retailer_moniker, customer_id, cust_preferences, created_datetime, modified_datetime)
+
+def __save(db, id, retailer_moniker, customer_id, cust_preferences, created_datetime, modified_datetime):
 
     # Make a string version of JSON to store:
     customer_pref_json = json.dumps(cust_preferences)
@@ -154,7 +175,7 @@ def saveCustomerPreference(retailer_moniker, customer_id, cust_preferences):
         ReturnValues='NONE'
     )
 
-def findCustomerPreference(key):
+def __findCustomerPreference(key):
     db = boto3.client('dynamodb');
 
     response = db.get_item(
@@ -172,79 +193,79 @@ def findCustomerPreference(key):
         ]
     )
 
-    result = populateRecordFromDynamoDB(response)
+    result = __populateRecordFromDynamoDB(response)
 
     return result
 
-def validateCustomerPreferences(cust_preferences_dict):
+def __validateCustomerPreferences(cust_preferences_dict):
     error_msgs = []
 
     # No point in checking further if this check fails:
     if not cust_preferences_dict:
-        error_msgs.append(VALIDATION_MSG_CUST_PREFS)
+        error_msgs.append(ResponseMessage('ERROR', None, 'customer_preferences', VALIDATION_MSG_CUST_PREFS))
         raise InputValidationException(error_msgs)
 
     if(not cust_preferences_dict.get('first_name')):
-        error_msgs.append(VALIDATION_MSG_FIRST_NAME)
+        error_msgs.append(ResponseMessage('ERROR', None, 'first_name', VALIDATION_MSG_FIRST_NAME))
     if(not cust_preferences_dict.get('last_name')):
-        error_msgs.append(VALIDATION_MSG_LAST_NAME)
+        error_msgs.append(ResponseMessage('ERROR', None, 'last_name', VALIDATION_MSG_LAST_NAME))
 
     notification_preferences = cust_preferences_dict.get('notification_pref')
     notification_preferences_details = cust_preferences_dict.get('notification_pref_details')
 
     # Notification Preference and Notification Preferences Details lists cannot be null (but can be empty)
     if(notification_preferences == None):
-        error_msgs.append(VALIDATION_MSG_NOTIFICATION_PREFS)
+        error_msgs.append(ResponseMessage('ERROR', None, 'notification_pref', VALIDATION_MSG_NOTIFICATION_PREFS))
         raise InputValidationException(error_msgs)
     else:
         notification_preferences = sorted(notification_preferences)
 
     if(notification_preferences_details == None):
-        error_msgs.append(VALIDATION_MSG_NOTIFICATION_PREFS_DETAILS)
+        error_msgs.append(ResponseMessage('ERROR', None, 'notification_pref_details', VALIDATION_MSG_NOTIFICATION_PREFS_DETAILS))
         raise InputValidationException(error_msgs)
     notification_pref_details_sorted = sorted(notification_preferences_details, key=lambda k: k['name'])
 
     # Notification Preferences and Notification Preferences Details must be of the same length:
     if len(notification_preferences) != len(notification_pref_details_sorted):
-        error_msgs.append(VALIDATION_MSG_NOTIFICATION_PREFS_AND_DETAILS_MISMATCH_LENGTH)
+        error_msgs.append(ResponseMessage('ERROR', None, None, VALIDATION_MSG_NOTIFICATION_PREFS_AND_DETAILS_MISMATCH_LENGTH))
 
     # Loop through the channels and verify that the corresponding item is present in details:
     idx = 0
     for item in notification_preferences:
         # Check for allowed value options:
         if not item or not (item == NOTIFICATION_PREF_SMS or item == NOTIFICATION_PREF_FB or item == NOTIFICATION_PREF_EMAIL):
-            error_msgs.append(VALIDATION_MSG_NOTIFICATION_PREFS_CHANNEL)
+            error_msgs.append(ResponseMessage('ERROR', None, 'notification_pref', VALIDATION_MSG_NOTIFICATION_PREFS_CHANNEL))
 
         # Make sure that there is a corresponding option for Notification Preferences Details:
         details = notification_pref_details_sorted[idx]
         if not details or item != details.get('name') or not details.get('value'):
-            error_msgs.append(VALIDATION_MSG_NOTIFICATION_PREFS_AND_DETAILS_MISMATCH_ITEM + item)
+            error_msgs.append(ResponseMessage('ERROR', None, 'notification_pref_details', VALIDATION_MSG_NOTIFICATION_PREFS_AND_DETAILS_MISMATCH_ITEM + item))
 
         idx += 1
 
     # Validate address fields:
     address = cust_preferences_dict.get('address')
     if(not address):
-        error_msgs.append(VALIDATION_MSG_ADDRESS)
+        error_msgs.append(ResponseMessage('ERROR', None, 'address', VALIDATION_MSG_ADDRESS))
         raise InputValidationException(error_msgs)
     if not address.get('street_1'):
-        error_msgs.append(VALIDATION_MSG_ADDRESS_STREET1)
+        error_msgs.append(ResponseMessage('ERROR', None, 'street_1', VALIDATION_MSG_ADDRESS_STREET1))
     if not address.get('city'):
-        error_msgs.append(VALIDATION_MSG_ADDRESS_CITY)
+        error_msgs.append(ResponseMessage('ERROR', None, 'city', VALIDATION_MSG_ADDRESS_CITY))
     if not address.get('state'):
-        error_msgs.append(VALIDATION_MSG_ADDRESS_STATE)
+        error_msgs.append(ResponseMessage('ERROR', None, 'state', VALIDATION_MSG_ADDRESS_STATE))
     if not address.get('zip'):
-        error_msgs.append(VALIDATION_MSG_ADDRESS_ZIP)
+        error_msgs.append(ResponseMessage('ERROR', None, 'zip', VALIDATION_MSG_ADDRESS_ZIP))
     if not address.get('country'):
-        error_msgs.append(VALIDATION_MSG_ADDRESS_COUNTRY)
+        error_msgs.append(ResponseMessage('ERROR', None, 'country', VALIDATION_MSG_ADDRESS_COUNTRY))
 
     if len(error_msgs) > 0:
         raise InputValidationException(error_msgs)
 
-def makeKey(retailer_moniker, customer_id):
+def __makeKey(retailer_moniker, customer_id):
     return ''.join([retailer_moniker, keyDelim, customer_id])
 
-def makeCustomerDetails(response):
+def __makeCustomerDetails(response):
     result = {}
 
     if response == None:
@@ -263,7 +284,7 @@ def makeCustomerDetails(response):
 
     return result
 
-def populateRecordFromDynamoDB(response):
+def __populateRecordFromDynamoDB(response):
     result = {}
 
     if response == None or response.get('Item') == None:
@@ -285,32 +306,47 @@ def populateRecordFromDynamoDB(response):
 
     return result
 
-def makeResponse(customer_preferences, exception, event):
-    statusCode = 200
+def __makeResponse(customer_preferences, exception, event):
+    status = 'success'
+    status_code = 200
+
     body = {}
-    if not exception:
-        body['message'] = "API call executed successfully."
-    else:
-        body['message'] = "API call failed!"
+    body['status'] = status
+    body['status_code'] = status_code
+    body['messages'] = []
+
+    if exception:
+        status = 'error'
+        status_code = 400
+        body['status'] = status
+
         if type(exception) == InputValidationException:
-            statusCode = exception.responseCode
-            #messages = []
-            #messages.append('zaebali')
-            #messages.append('uzhe vse')
-            body['exception_messages'] = []
-            for msg in exception.messages:
-                body['exception_messages'].append(msg)
+            status_code = exception.responseCode
+            body['status_code'] = status_code
+            for msg in exception.response_messages:
+                message = {}
+                message['level'] = msg.level
+                message['code'] = msg.code
+                message['field'] = msg.field
+                message['message'] = msg.message
+                body['messages'].append(message)
         else:
-            statusCode = 500
-            body['exception_messages'] = exception.args[0]
-            body['warning_messages'] = 'standard exception'
+            status_code = 500
+            body['status_code'] = status_code
+            message = {}
+            message['level'] = 'ERROR'
+            message['code'] = None
+            message['field'] = None
+            message['message'] = exception.args[0]
+            body['messages'].append(message)
 
     body['customer_preferences'] = customer_preferences
     if(event):
         body['input'] = event
 
     response = {
-        "statusCode": statusCode,
+        #"statusCode": status_code,
+        "statusCode": 200,
         "body": json.dumps(body)
     }
     return response
